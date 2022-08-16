3ab8d88f4568d647070681c52b98525b9ead5f24
thisisMoe <bounoua.smm@gmail.com>
2022-08-16 23:37:31 +0100

    Add the Links to the Sidebar and the Route config to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/layout/Nav';
+import Board from "./routes/Board";
+import Stats from "./routes/Stats";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
           <Nav />
+        <Routes>
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/board" element={<Board />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Sidebar({ show, toggleSidebar }) {
   return (
     show && (
@@ -8,14 +10,18 @@ export default function Sidebar({ show, toggleSidebar }) {
         ></div>
         <div className="bg-[#fafafa] overflow-y-scroll z-30 fixed transition duration-300 w-[300px] opacity-100 h-full flex-col px-1 pt-8">
           <div className="border-b bg-indigo-100 hover:bg-indigo-200 mb-[2px]">
+            <Link onClick={toggleSidebar} to="/stats">
               <div className="uppercase font-bold py-2 px-4 cursor-pointer flex items-center gap-4">
                 <div>Stats</div>
               </div>
+            </Link>
           </div>
           <div className="border-b bg-indigo-100 hover:bg-indigo-200">
+            <Link onClick={toggleSidebar} to="/board">
               <div className="uppercase font-bold py-2 px-4 cursor-pointer flex items-center gap-4">
                 <div>Board</div>
               </div>
+            </Link>
           </div>
         </div>
       </div>
