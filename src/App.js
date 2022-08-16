606b174cc3a3eca51a0a7ef58df66482743089c0
thisisMoe <bounoua.smm@gmail.com>
2022-08-16 23:31:12 +0100

    Install react-router + connect your app to the browser's URL (BrowserRouter) and add 2 routes (src/Stats.js + src/Board.js)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Nav from './components/layout/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <BrowserRouter>
+          <Nav />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,10 +7,14 @@ export default function Sidebar({ show, toggleSidebar }) {
           onClick={toggleSidebar}
         ></div>
         <div className="bg-[#fafafa] overflow-y-scroll z-30 fixed transition duration-300 w-[300px] opacity-100 h-full flex-col px-1 pt-8">
-
-          <div className="border-b bg-indigo-100 hover:bg-indigo-200">
+          <div className="border-b bg-indigo-100 hover:bg-indigo-200 mb-[2px]">
               <div className="uppercase font-bold py-2 px-4 cursor-pointer flex items-center gap-4">
                 <div>Stats</div>
+              </div>
+          </div>
+          <div className="border-b bg-indigo-100 hover:bg-indigo-200">
+              <div className="uppercase font-bold py-2 px-4 cursor-pointer flex items-center gap-4">
+                <div>Board</div>
               </div>
           </div>
         </div>
